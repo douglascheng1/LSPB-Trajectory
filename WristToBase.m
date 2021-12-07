00f05eccfa39e942019c1281f0366bdead448b78
pahulmangat <70053740+pahulmangat@users.noreply.github.com>
2021-12-07 17:05:57 -0500

    Update WristToBase.m

--- WristToBase.m
+++ WristToBase.m
@@ -2,7 +2,7 @@ function transformationProduct = WristToBase(dhParameters)
 %wristToBase computes the homogenous transformation that relates he wrist
 %frame to the base frane, given the DH parameters for an N DOF manipulator
 
-    frameCount = size(dhParameters,1); %get # of frames by getting the # of rows of the DH table
+    frameCount = size(dhParameters,1); %get # of frames from # of rows of the DH table
     
     for i=1:frameCount %iterate through each frame
         % Extract DH parameters for each frame
@@ -10,15 +10,14 @@ function transformationProduct = WristToBase(dhParameters)
         a = dhParameters(i,2);
         d = dhParameters(i,3);
         theta = dhParameters(i,4);
-    
            
-        transformMatrix = [ %calcuate Tranformation Matrix for frame
+        transformMatrix = [ % calculate intermediate Transformation Matrix
             cosd(theta), -sind(theta), 0, a;
             sind(theta)*cosd(alpha), cosd(theta)*cosd(alpha), -sind(alpha), -sind(alpha)*d;
             sind(theta)*sind(alpha), cosd(theta)*sind(alpha), cosd(alpha), cosd(alpha)*d;
             0, 0, 0, 1 ];
         
-        % calculate each intermediate Transformation Matrix
+        % Transformation Matrix products
         if i == 1
                 transformationProduct = transformMatrix;
         else
