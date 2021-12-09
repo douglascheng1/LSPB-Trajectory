918d71662ab8bca7049653512c907b1fd36f6ccf
pahulmangat <70053740+pahulmangat@users.noreply.github.com>
2021-12-08 21:21:47 -0500

    Update CPSTrajectory.m
    added comments

--- CPSTrajectory.m
+++ CPSTrajectory.m
@@ -4,38 +4,38 @@ p = [0,-8,-90;0,45,90;0,0.2,0.4]; % Define Positions
 v = [0,10,0;0,40,0;0,0.2,0]; % Define Velocities
 t = [0, 2, 4];
 
-totalPosition = [];
-totalVelocity = [];
-totalAcceleration = [];
+totalPosition = []; %intialize position array
+totalVelocity = []; %intialize velocity array
+totalAcceleration = []; %intialize acceleration array
 
-sample = 0.1;
+sample = 0.1 % sampe time period
 
-for i = 1:size(p)
-    for j=1:(length(t)-1)
+for i = 1:size(p) % iterate through # of joints
+    for j=1:(length(t)-1) % iterate through time peroids
         j
         a_0 = p(i,j) %a0 coefficient
         a_1 = v(i,j); %a1 coefficient
-        a_2 = (3*(p(i,j+1) - p(i,j)) - (t(j+1)-t(j))*(2*v(i,j)+v(i,j+1)))/((t(j+1)-t(j))^2);
+        a_2 = (3*(p(i,j+1) - p(i,j)) - (t(j+1)-t(j))*(2*v(i,j)+v(i,j+1)))/((t(j+1)-t(j))^2); %a1 coefficient
         a_3 = ((2*(p(i,j) - p(i,j+1))) + (t(j+1)-t(j))*(v(i,j)+v(i,j+1)))/((t(j+1)-t(j))^3); %a3 coefficient
 
-        ts = (t(j):sample:t(j+1))-t(j);
+        ts = (t(j):sample:t(j+1))-t(j); % sample time within time peroid
 
-        theta = a_0 + a_1*ts + a_2*ts.^2 + a_3*ts.^3; %position function
-        dtheta = a_1 + 2*a_2*ts + 3*a_3*ts.^2; %velocity function
-        ddtheta = 2*a_2 + 6*a_3*ts; %acceleration function
+        pos = a_0 + a_1*ts + a_2*ts.^2 + a_3*ts.^3; %position function
+        vel = a_1 + 2*a_2*ts + 3*a_3*ts.^2; %velocity function
+        acc = 2*a_2 + 6*a_3*ts; %acceleration function
 
-        totalPosition = [totalPosition, theta];
-        totalVelocity = [totalVelocity, dtheta];
-        totalAcceleration = [totalAcceleration, ddtheta];
+        totalPosition = [totalPosition, pos]; % update position array
+        totalVelocity = [totalVelocity, vel]; % update velocity array
+        totalAcceleration = [totalAcceleration, acc]; % update acceleration array
 
-        if j ~= (length(t)-1)
+        if j ~= (length(t)-1) % delete last element of trajectory array so next time peroid can start there
             totalPosition(end) = [];
             totalVelocity(end) = [];
             totalAcceleration(end) = [];
-        else
-            time = t(1):sample:t(length(t));
-            %plot position
+        else %plot trajectory
+            time = t(1):sample:t(length(t)); % sample tiem over entire time period
             figure
+            %plot position
             subplot(3,1,1);
             plot(time, totalPosition)
             title(sprintf('Position of Joint %d',i));
@@ -51,7 +51,8 @@ for i = 1:size(p)
             title(sprintf('Acceleration of Joint %d',i));
             grid on
             
-            totalPosition = [];
+            % reset trajectory arrays fpr next joint
+            totalPosition = []; 
             totalVelocity = [];
             totalAcceleration = [];
         end
