9a982461e82d1c1fc2687c26bb685ffe4ad345b9
pahulmangat <70053740+pahulmangat@users.noreply.github.com>
2021-12-08 21:25:24 -0500

    Update CPSTrajectory.m

--- CPSTrajectory.m
+++ CPSTrajectory.m
@@ -8,11 +8,11 @@ totalPosition = []; %intialize position array
 totalVelocity = []; %intialize velocity array
 totalAcceleration = []; %intialize acceleration array
 
-sample = 0.1 % sampe time period
+sample = 0.1; % sampe time period
 
 for i = 1:size(p) % iterate through # of joints
     for j=1:(length(t)-1) % iterate through time peroids
-        a_0 = p(i,j) %a0 coefficient
+        a_0 = p(i,j); %a0 coefficient
         a_1 = v(i,j); %a1 coefficient
         a_2 = (3*(p(i,j+1) - p(i,j)) - (t(j+1)-t(j))*(2*v(i,j)+v(i,j+1)))/((t(j+1)-t(j))^2); %a2 coefficient
         a_3 = ((2*(p(i,j) - p(i,j+1))) + (t(j+1)-t(j))*(v(i,j)+v(i,j+1)))/((t(j+1)-t(j))^3); %a3 coefficient
